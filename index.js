6ece153d82bef2cc6b13992e81c6f88f97f5fe90
unknown <emprenderusinque@hotmail.com>
2022-11-28 16:00:41 -0500

    Configuring Express

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('Hola !!')
+const express = require('express');
+
+// Create the express server 
+
+const app = express();
+
+// Routes
+
+app.get('/', ( req, res ) => {
+    
+    console.log('Se requiere /');
+    res.json({
+        ok: true
+    })
+});
+
+
+// listen requests
+
+app.listen( 4000, () => {
+    console.log(`Servidor correindo en puerto ${4000}`)
+});
+
