ce2c447483229bb55820fa53d5a79d6e1ec75400
unknown <emprenderusinque@hotmail.com>
2022-11-29 14:03:55 -0500

    environment variables

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 const express = require('express');
+require('dotenv').config();
+
 
 // Create the express server 
 
 const app = express();
 
+
+// public diretory
+
+app.use( express.static('public'))
+
 // Routes
 
-app.get('/', ( req, res ) => {
+// app.get('/', ( req, res ) => {
     
-    console.log('Se requiere /');
-    res.json({
-        ok: true
-    })
-});
+//     console.log('Se requiere /');
+//     res.json({
+//         ok: true
+//     })
+// });
 
 
 // listen requests
 
-app.listen( 4000, () => {
-    console.log(`Servidor correindo en puerto ${4000}`)
+app.listen( process.env.PORT, () => {
+    console.log(`Servidor correindo en puerto ${ process.env.PORT }`)
 });
 
