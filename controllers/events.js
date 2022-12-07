2f58af52d91db48877c8f1b2f019fa82816a0034
unknown <emprenderusinque@hotmail.com>
2022-12-06 21:04:54 -0500

    delete Event

--- controllers/events.js
+++ controllers/events.js
@@ -78,7 +78,7 @@ const updateEvent = async( req, res = response ) => {
         });
     } catch (error) {
         console.log(error);
-        res.status(500).json({
+        return res.status(500).json({
             ok: false,
             msg: 'Hable con el administrador'
         })
@@ -87,12 +87,43 @@ const updateEvent = async( req, res = response ) => {
 }
 
 
-const deleteEvent = ( req, res = response ) => {
-    
-    res.json({
-        ok: true,
-        msg: 'deleteEvent'
-    });
+const deleteEvent = async( req, res = response ) => {
+    const eventoId = req.params.id;
+    const uid = req.uid
+
+    try {
+        
+        const evento = await Evento.findById( eventoId );
+
+        if( !evento ) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Evento no existe con ese id'
+            })
+        }
+
+        if( evento.user.toString() !== uid ) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegio de elimnar este evento'
+            });
+        }
+
+
+       await Evento.findByIdAndDelete(eventoId);
+
+        
+        res.json({
+            ok: true,
+            evento: eventoActualizado
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        })
+    }
 }
 
 
